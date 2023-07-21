1d8514b6fe0662058f47a507826d115d0ed5f9f0
jaspopejoy <120056960+jaspopejoy@users.noreply.github.com>
2023-07-20 21:50:05 -0500

    fixing errors

---
+++ Services/Factories/BTUserClaimsPrincipalFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using BugTracker.Models;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+namespace BugTracker.Services.Factories
+{
+	public class BTUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<BTUser, IdentityRole>
+	{
+		public BTUserClaimsPrincipalFactory(UserManager<BTUser> userManager, RoleManager<IdentityRole> roleManager, IOptions<IdentityOptions> optionsAccessor) : base(userManager, roleManager, optionsAccessor)
+		{
+		}
+
+		protected override async Task<ClaimsIdentity> GenerateClaimsAsync(BTUser user)
+		{
+			ClaimsIdentity identity = await base.GenerateClaimsAsync(user);
+			identity.AddClaim(new Claim("CompanyId", user.CompanyId.ToString()));
+
+			return identity;
+		}
+	}
+}
